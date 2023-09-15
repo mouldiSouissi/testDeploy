3168a481c124fb2738f60a34bd3af80e1cd034ff
Mouldi Souissi <so.mouldi@FIRST.LOCAL>
2023-09-15 12:12:23 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <div className="card">
-          <h1>Hello github</h1>
+          <h1>Hello github actions</h1>
         </div>
       </div>
     </>
